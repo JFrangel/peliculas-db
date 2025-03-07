2e675b547f1f04462b16a850d2b79a63b7e654a5
Jose Frangel Padilla Camacho <166357520+JFrangel@users.noreply.github.com>
2025-03-07 00:33:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const repoOwner = "JFrangel";
 const repoName = "peliculas-db";
 const jsonFilePath = "movies.json";
-const accessToken = "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";   // ¡Reemplázalo con tu token real!
+const accessToken = "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";   // ¡Reemplázalo con tu token real!
 
 document.addEventListener('DOMContentLoaded', async function () {
     const menuToggle = document.querySelector('.menu-toggle');
